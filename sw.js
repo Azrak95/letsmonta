eaec51f684945184729fabe9e4e6412fccbb3a1d
Azrak95 <monirelazrak@gmail.com>
2026-06-10 10:11:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,38 @@
-const CACHE = 'letsmonta-v1';
+const CACHE = 'letsmonta-v3';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/app_icon.png',
-  '/avatars/Monir.png',
-  '/avatars/Bego.png',
-  '/avatars/David.png',
-  '/avatars/Bea.png',
-  '/avatars/Miriam_M.png',
-  '/avatars/Edu.png',
-  '/avatars/Miriam_G.png',
-  '/avatars/Pincho.png',
-  '/avatars/Pipe.png',
-  '/avatars/Sheyla.png',
-  '/avatars/Maria.png',
-  '/avatars/Kiki.png',
-  '/avatars/Idoia.png',
+  './',
+  './index.html',
+  './manifest.json',
+  './app_icon.png',
+  './bg_monta.png',
+  './avatars/Monir.png',
+  './avatars/Bego.png',
+  './avatars/David.png',
+  './avatars/Bea.png',
+  './avatars/Miriam.png',
+  './avatars/Edu.png',
+  './avatars/MiriamG.png',
+  './avatars/Pincho.png',
+  './avatars/Pipe.png',
+  './avatars/Sheyla.png',
+  './avatars/Maria.png',
+  './avatars/Kiki.png',
+  './avatars/Idoia.png',
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
