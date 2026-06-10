74a2247a3522e7f773f24997fe8f1e327fa9d0b6
Azrak95 <monirelazrak@gmail.com>
2026-06-10 10:51:00 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAg6VpZZSlfS7zXuH8aklndJKgLlM1SmCM",
+  authDomain: "letsmonta.firebaseapp.com",
+  databaseURL: "https://letsmonta-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "letsmonta",
+  storageBucket: "letsmonta.firebasestorage.app",
+  messagingSenderId: "827368602785",
+  appId: "1:827368602785:web:d6df974b604243579d04e0"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  const { title, body, icon } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: icon || '/letsmonta/app_icon.png',
+    badge: '/letsmonta/app_icon.png',
+    vibrate: [200, 100, 200],
+  });
+});
